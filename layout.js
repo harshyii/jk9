f19f7c5861c148359cb78d2d2b865104a7c31678
Gala <makeagoodfriend@gmail.com>
2026-07-15 10:58:53 +0530

    major changes to ui

--- layout.js
+++ layout.js
@@ -43,11 +43,6 @@ id="navbarMain">
 
 <ul class="navbar-nav me-lg-4">
 
-<li class="nav-item">
-<a class="nav-link" href="#/">
-Home
-</a>
-</li>
 
 <li class="nav-item">
 <a class="nav-link" href="#/products">
